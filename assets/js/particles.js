68b03365cba21572896180c9eb4e50ec5c76293c
alenk89 <77924959+alenk89@users.noreply.github.com>
2024-02-04 08:55:18 +0800

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,23 +1,23 @@
-{
+particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 12,
+      "value": 380,
       "density": {
         "enable": true,
         "value_area": 800
       }
     },
     "color": {
-      "value": "#1b1e34"
+      "value": "#ffffff"
     },
     "shape": {
-      "type": "polygon",
+      "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#000"
+        "color": "#000000"
       },
       "polygon": {
-        "nb_sides": 6
+        "nb_sides": 5
       },
       "image": {
         "src": "img/github.svg",
@@ -26,8 +26,8 @@
       }
     },
     "opacity": {
-      "value": 0.3,
-      "random": true,
+      "value": 0.5,
+      "random": false,
       "anim": {
         "enable": false,
         "speed": 1,
@@ -36,25 +36,25 @@
       }
     },
     "size": {
-      "value": 160,
+      "value": 3,
       "random": true,
       "anim": {
-        "enable": true,
-        "speed": 10,
-        "size_min": 40,
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
         "sync": false
       }
     },
     "line_linked": {
-      "enable": false,
-      "distance": 200,
+      "enable": true,
+      "distance": 150,
       "color": "#ffffff",
-      "opacity": 1,
-      "width": 2
+      "opacity": 0.4,
+      "width": 1
     },
     "move": {
       "enable": true,
-      "speed": 8,
+      "speed": 6,
       "direction": "none",
       "random": false,
       "straight": false,
@@ -71,18 +71,18 @@
     "detect_on": "canvas",
     "events": {
       "onhover": {
-        "enable": false,
+        "enable": true,
         "mode": "grab"
       },
       "onclick": {
-        "enable": false,
+        "enable": true,
         "mode": "push"
       },
       "resize": true
     },
     "modes": {
       "grab": {
-        "distance": 400,
+        "distance": 140,
         "line_linked": {
           "opacity": 1
         }
@@ -107,4 +107,25 @@
     }
   },
   "retina_detect": true
-}
+});
+
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
